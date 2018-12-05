a1c1fecc58a92ee13a93569f7340f1673bfb35b5
dell_local_repo <filip.szpunar@gmail.com>
2018-12-05 23:30:34 +0100

    db scripts

---
+++ db_scripts/00_tables.sql
@@ -0,0 +1,58 @@
+﻿--drop table [Task];
+--GO
+--drop table [TaskCategory];
+--GO
+--drop table [User];
+--GO
+
+CREATE TABLE [User] (
+  [Id] int IDENTITY (1,1)  NOT NULL,
+  [Login] nvarchar(20)  NOT NULL,
+  [FullName] nvarchar(50),
+  [Password] nvarchar(255)  NOT NULL,
+  [LastLoginDate] datetime
+);
+GO
+ALTER TABLE [User] ADD CONSTRAINT [PK_UserId] PRIMARY KEY ([Id]);
+GO
+
+CREATE TABLE [TaskCategory] (
+  [Id] int IDENTITY (1,1)  NOT NULL,
+  [CategoryName] nvarchar(50)  NOT NULL,
+  [Description] nvarchar(1000)
+);
+GO
+ALTER TABLE [TaskCategory] ADD CONSTRAINT [PK_TaskCategoryId] PRIMARY KEY ([Id]);
+GO
+
+
+CREATE TABLE [Task] (
+  [Id] int IDENTITY (1,1)  NOT NULL,
+  [TaskName] nvarchar(100)  NOT NULL,
+  [CreateDate] datetime NOT NULL DEFAULT GETDATE(),
+  [User_Id] int NOT NULL,
+  [TaskCategory_Id] int NOT NULL,
+  [Description] nvarchar(2000),
+  [TaskDate] datetime NOT NULL,
+  [Completed] int NOT NULL DEFAULT 0  
+);
+GO
+ALTER TABLE [Task] ADD CONSTRAINT [PK_TaskId] PRIMARY KEY ([Id]);
+GO
+
+ALTER TABLE [Task]     
+ADD CONSTRAINT FK_Task_UserId FOREIGN KEY (User_Id)     
+    REFERENCES [User] (Id) ;
+GO
+ALTER TABLE [Task]     
+ADD CONSTRAINT FK_Task_CategoryId FOREIGN KEY (TaskCategory_Id)     
+    REFERENCES [TaskCategory] (Id);
+GO
+
+
+insert into [TaskCategory]([CategoryName],[Description]) values ('Praca','Kategoria z zadaniami związanymi z pracą');
+GO
+insert into [TaskCategory]([CategoryName],[Description]) values ('Domowe','Kategoria z zadaniami związanymi domem');
+GO
+insert into [TaskCategory]([CategoryName],[Description]) values ('Inne','Różne zadania bez kategorii');
+
